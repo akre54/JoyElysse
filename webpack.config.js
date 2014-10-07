0dd4ea8d85d08e7cedba8835578b9c0de951f6e6
Adam Krebs <amk528@cs.nyu.edu>
2014-10-06 23:53:39 -0400

    new simpler layout

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,8 @@ module.exports = {
       { test: /\.woff$/,   loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
       { test: /\.ttf$/,    loader: 'file-loader?prefix=font/' },
       { test: /\.eot$/,    loader: 'file-loader?prefix=font/' },
-      { test: /\.svg$/,    loader: 'file-loader?prefix=font/' },
-    ],
-    noParse: /\.min\.js/
+      { test: /\.svg$/,    loader: 'file-loader?prefix=font/' }
+    ]
   },
   resolve: {
     modulesDirectories: ['bower_components', 'node_modules'],
