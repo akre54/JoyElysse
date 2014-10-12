d06b98c35d2d1620787865727751c8026c918691
Adam Krebs <amk528@cs.nyu.edu>
2014-10-12 02:50:19 -0400

    go back to old style of build. no need for fancy webpack

--- app/js/app.coffee
+++
@@ -1,3 +0,0 @@
-require 'normalize.css'
-# require 'font-awesome/css/font-awesome.css'
-require '../css/app.styl'
--- build.js
+++ build.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 
-var webpack = require('webpack'),
-    webpackConfig = require('./webpack.config'),
+var fs = require('fs'),
     del = require('del'),
     mkdirp = require('mkdirp'),
+    ncp = require('ncp'),
+    stylus = require('stylus'),
     rimraf = require('rimraf');
 
-var publicDir = './public';
-
-if (process.argv[2] ===  '--watch') webpackConfig.watch = true;
+var appDir = __dirname + '/app',
+    publicDir = __dirname + '/public',
+    cssDir = publicDir + '/css',
+    appStyl = __dirname + '/app/css/app.styl',
+    appCss = cssDir + '/app.css';
 
 del([publicDir], function() {
-  mkdirp(publicDir, function() {
-    webpack(webpackConfig, function(err, stats) {
-      console.log(stats.toString());
-    });
+  mkdirp.sync(publicDir);
+  ncp(appDir, publicDir, {filter: function(file) { return !/.(styl|coffee)$/.test(file); }}, function() {
+    stylus(fs.readFileSync(appStyl, 'utf8'))
+      .set('filename', appStyl)
+      .include(require('nib').path)
+      .render(function(err, css) {
+        if (err) console.log(err);
+        fs.createWriteStream(appCss).write(css);
+      });
   });
 });
