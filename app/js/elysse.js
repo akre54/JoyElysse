d09619525dc3e6cf78c004aff730586ac9e38ad1
Adam Krebs <amk528@cs.nyu.edu>
2014-10-21 17:43:44 -0400

    get close button to work

--- app/js/elysse.js
+++ app/js/elysse.js
@@ -2,12 +2,18 @@
 
   var each = [].forEach;
 
+  var toggleCloseBtn = function() {
+    var hide = (location.hash === '#home' || location.hash === '');
+    document.getElementById('close-btn').style.display = hide ? 'none' : 'block';
+  }
+
+  window.addEventListener('hashchange', toggleCloseBtn);
+  toggleCloseBtn();
+
 
   // // Change pages
   // each.call(document.querySelectorAll('.nav-link'), function(link) {
   //   link.addEventListener('click', function(e) {
-  //     e.preventDefault();
-
   //     each.call(['shows', 'mailing-list', 'contact'], function(page) {
   //       document.body.classList.toggle(page + '-page', link.href === page);
   //     });
