f7c2b6a53b99e3a517f6a33f2083dda75b7b08ce
Adam Krebs <amk528@cs.nyu.edu>
2014-10-21 14:20:42 -0400

    nest pages as divs

--- app/js/elysse.js
+++ app/js/elysse.js
@@ -3,15 +3,15 @@
   var each = [].forEach;
 
 
-  // Change pages
-  each.call(document.querySelectorAll('.nav-link'), function(link) {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
+  // // Change pages
+  // each.call(document.querySelectorAll('.nav-link'), function(link) {
+  //   link.addEventListener('click', function(e) {
+  //     e.preventDefault();
 
-      each.call(['shows', 'mailing-list', 'contact'], function(page) {
-        document.body.classList.toggle(page + '-page', link.href === page);
-      });
-    });
-  });
+  //     each.call(['shows', 'mailing-list', 'contact'], function(page) {
+  //       document.body.classList.toggle(page + '-page', link.href === page);
+  //     });
+  //   });
+  // });
 
 })();
