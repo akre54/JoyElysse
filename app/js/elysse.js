cd8854125cd0904c000d4af0926a05b4acf71e31
Adam Krebs <amk528@cs.nyu.edu>
2014-10-21 23:16:04 -0400

    scroll page within viewbox

--- app/js/elysse.js
+++ app/js/elysse.js
@@ -5,19 +5,27 @@
   var toggleCloseBtn = function() {
     var hide = (location.hash === '#home' || location.hash === '');
     document.getElementById('close-btn').style.display = hide ? 'none' : 'block';
+
+    // var previous = document.querySelector('.active');
+    // previous && previous.classList.remove('active');
+
+    // var id = location.hash.match(/#(.*)/)[1];
+    // document.getElementById(id).classList.add('active');
   }
 
   window.addEventListener('hashchange', toggleCloseBtn);
   toggleCloseBtn();
 
 
-  // // Change pages
-  // each.call(document.querySelectorAll('.nav-link'), function(link) {
-  //   link.addEventListener('click', function(e) {
-  //     each.call(['shows', 'mailing-list', 'contact'], function(page) {
-  //       document.body.classList.toggle(page + '-page', link.href === page);
-  //     });
-  //   });
-  // });
+  // Change pages
+  each.call(document.querySelectorAll('.nav-link'), function(link) {
+    link.addEventListener('click', function(e) {
+      var previous = document.querySelector('.active');
+      previous && previous.classList.remove('active');
+
+      var id = e.currentTarget.href.match(/#(.*)/)[1] || 'home';
+      document.getElementById(id + '-page').classList.add('active');
+    });
+  });
 
 })();
