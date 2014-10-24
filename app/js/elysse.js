da108e34b1d84712aea98655c84e953a90581e50
Adam Krebs <amk528@cs.nyu.edu>
2014-10-23 21:27:19 -0400

    bring in contact form and email stuff

--- app/js/elysse.js
+++ app/js/elysse.js
@@ -14,16 +14,8 @@
   window.addEventListener('hashchange', setActivePage);
   setActivePage();
 
-
-  // Change pages
-  each.call(document.querySelectorAll('.nav-link'), function(link) {
-    link.addEventListener('click', function(e) {
-      // var previous = document.querySelector('.active');
-      // previous && previous.classList.remove('active');
-
-      // var id = e.currentTarget.href.match(/#(.*)/)[1] || 'home';
-      // document.getElementById(id + '-page').classList.add('active');
-    });
+  document.getElementById('signup').addEventListener('submit', function() {
+    debugger
+    return false;
   });
-
 })();
