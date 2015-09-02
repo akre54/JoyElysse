6af7d25f27c8122a71bda32ddd1e1b100e9700a3
Adam Krebs <amk528@cs.nyu.edu>
2015-09-02 10:28:41 -0400

    better active links

--- app/js/elysse.js
+++ app/js/elysse.js
@@ -4,20 +4,20 @@ require('file?name=favicon.ico!../assets/images/favicon.ico')
 var each = [].forEach
 
 var setActivePage = function() {
-  var previous = document.querySelector('.active')
-  previous && previous.classList.remove('active')
+  each.call(document.querySelectorAll('.active'), function(el) { el.classList.remove('active') })
 
   var loc = (location.hash || '#home')
   var id = loc + '-page'
   document.querySelector(id).classList.add('active')
 
-  each.call(document.querySelectorAll('.page__links a'), function(el) { el.classList.remove('active') })
-  document.querySelector('.page__links a[href="'+ loc + '"]').classList.add('active')
-
   document.body.className = document.body.className.replace(/\b\w+-page\b/, '').trim()
   document.body.classList.add(id.slice(1))
 
-  if (id === '#home-page') window.history.replaceState(null, document.title, '/')
+  if (loc === '#home') {
+    window.history.replaceState(null, document.title, '/')
+  } else {
+    document.querySelector('.page__links a[href="'+ loc + '"]').classList.add('active')
+  }
 }
 
 window.addEventListener('hashchange', setActivePage, false)
